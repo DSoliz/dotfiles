e2ffe7a705c2e4796c62405b1a9f05b71b7ed6fb
Diego Soliz <zilosgodi@gmail.com>
2023-10-07 16:19:35 +1100

    initial config setup

---
+++ scripts/ubuntu.sh
@@ -0,0 +1,8 @@
+sudo apt install git
+sudo apt install tmux
+sudo apt install alacritty
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+curl https://get.volta.sh | bash
+gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+git config --global init.defaultBranch main
+
