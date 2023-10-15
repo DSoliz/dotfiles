1f67ffaa8d6e49f0540b11756fb91ccca7f2ebef
Diego Soliz <you@example.com>
2023-10-15 16:05:52 +1100

    add capslock switch to escape

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -5,4 +5,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 curl https://get.volta.sh | bash
 gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
 git config --global init.defaultBranch main
+# make capslock behave as escape
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
+git config --global core.editor hx
 
