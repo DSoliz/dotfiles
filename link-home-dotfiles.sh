33b197a3d35a90017dc4bb8bb8d9017079739cc2
Diego Soliz <zilosgodi@gmail.com>
2024-09-13 23:27:03 +1000

    update scripts

---
+++ link-home-dotfiles.sh
@@ -0,0 +1,4 @@
+# cp -r ./home/.config ~/.config
+# cp -r ./home/.local ~/.local
+# cp ./home/.zshrc ~/.zshrc
+cp -rsf ~/repos/dotfiles/home/. ~
--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -1,11 +1,19 @@
 sudo apt install git
-sudo apt install tmux
 sudo apt install alacritty
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-curl https://get.volta.sh | bash
-gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+
+snap install helix --classic
+git config --global core.editor hx
 git config --global init.defaultBranch main
+git config --global user.email "zilosgodi@gmail.com"
+git config --global user.name "Diego Soliz"
+
+sudo snap install zellij --classic
+
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# disable dock hotkeys
+gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
 # make capslock behave as escape
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
-git config --global core.editor hx
 
